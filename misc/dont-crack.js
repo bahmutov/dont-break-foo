dc45c3ed5360023e740efceab7d974f60bf20382
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-12-20 23:52:09 -0500

    feat(cracks): show current and next release code

--- misc/dont-crack.js
+++ misc/dont-crack.js
@@ -1,5 +1,18 @@
-function dontCrack () {
-  console.log('dont-crack arguments')
-  console.log(arguments)
+const log = require('debug')('dont-crack')
+
+function dontCrack (opts, config, callback) {
+  log('dont-crack arguments')
+  log('opts', opts)
+  log('commits')
+  log(config.commits)
+  log('last release was', config.lastRelease)
+  log('next release is', config.nextRelease)
+
+  if (config.nextRelease.type === 'major') {
+    log('major release will crack dependents by definition')
+    log('no need to check.')
+    return callback()
+  }
+  callback(new Error('Not yet'))
 }
 module.exports = dontCrack
